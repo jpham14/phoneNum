eb5b51489fae5d5c903a3cda4afb35691eb697db
jevuu <35980711+jevuu@users.noreply.github.com>
2018-02-05 14:43:15 -0500

    Added functionality that removes duplicate numbers.

--- src/net/codejava/parse.java
+++ src/net/codejava/parse.java
@@ -2,6 +2,7 @@ package net.codejava;
 
 import java.io.IOException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.servlet.ServletException;
@@ -13,13 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.i18n.phonenumbers.PhoneNumberMatch;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
 
 /**
  * Servlet implementation class parse
  */
-@WebServlet("/parse")
+@WebServlet(urlPatterns = {"/parse", "/parseFile"})
 public class parse extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	PhoneNumberUtil pUtil = PhoneNumberUtil.getInstance();
+
 	/**
      * @see HttpServlet#HttpServlet()
      */
@@ -32,18 +36,11 @@ public class parse extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		PhoneNumberUtil pUtil = PhoneNumberUtil.getInstance();
 		String qString = URLDecoder.decode(request.getQueryString(),"UTF-8");
 		
-		Iterator<PhoneNumberMatch> foundNum = pUtil.findNumbers(qString, "CA").iterator();
-		
-		response.getWriter().append("[");
-		while(foundNum.hasNext()) {
-			response.getWriter().append(pUtil.format(foundNum.next().number(), PhoneNumberFormat.NATIONAL))
-								.append((foundNum.hasNext())? ", " : "");
-		}
-		response.getWriter().append("]");
+		Iterator<PhoneNumberMatch> foundNumbers = pUtil.findNumbers(qString, "CA").iterator();
+		ArrayList<PhoneNumber> uniqueNumberList = removeDupeNumbers(foundNumbers);
+		sendResponse(uniqueNumberList, response);
 	}
 
 	/**
@@ -53,5 +50,26 @@ public class parse extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
+	
+	protected ArrayList<PhoneNumber> removeDupeNumbers(Iterator<PhoneNumberMatch> foundNumbers){
+		ArrayList<PhoneNumber> uniqueNumberList = new ArrayList<>();
+		
+		while(foundNumbers.hasNext()) {
+			PhoneNumber tempNum = foundNumbers.next().number();
+			if(!uniqueNumberList.contains(tempNum)) {
+				uniqueNumberList.add(tempNum);
+			}
+		}
+		return uniqueNumberList;
+	}
+	
+	protected void sendResponse(ArrayList<PhoneNumber> uniqueNumberList, HttpServletResponse response) throws ServletException, IOException{
+		response.getWriter().append("[");
+		
+		for(int i = 0; i < uniqueNumberList.size(); i++) {
+			response.getWriter().append(pUtil.format(uniqueNumberList.get(i), PhoneNumberFormat.NATIONAL))
+								.append((i+1 < uniqueNumberList.size())? ", " : "");
+		}
+		response.getWriter().append("]");
+	}
 }
